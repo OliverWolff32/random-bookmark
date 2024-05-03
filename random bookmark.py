17828a983ffb989b23f3d79d1c7c8edaf6c695a3
Oliver Wolff <oliverwolff@Olivers-MacBook-Air-2.local>
2024-05-03 10:51:45 -0700

    first commit

---
+++ random bookmark.py
@@ -0,0 +1,40 @@
+import json
+import webbrowser
+import random
+
+def printJson(dict):
+    print(json.dumps(dict, sort_keys=False, indent=2))
+
+windows_file_loc = r"C:\\Users\\tw\AppData\\Local\\Google\\Chrome\\User Data\Default\Bookmarks"
+mac_file_loc = "/Users/oliverwolff/Library/Application Support/Google/Chrome/Default/Bookmarks"
+
+raw_data = open(windows_file_loc, "r")
+object_data = json.loads(raw_data.read())
+
+del object_data["sync_metadata"]
+del object_data["checksum"]
+
+bookmarks = object_data["roots"]["bookmark_bar"]["children"]
+
+unused_folders = []
+urls = []
+
+
+for bookmark in bookmarks:
+    if "children" in bookmark:
+        unused_folders.append(bookmark["children"])
+    else:
+        urls.append(bookmark["url"])
+
+for object in unused_folders:
+   for bookmark in object:
+        if "children" in object:
+            unused_folders.append(bookmark["children"])
+        else:
+            urls.append(bookmark["url"])
+
+
+random_url = random.choice(urls)
+print(random_url)
+
+webbrowser.open(random_url)
